11c64f71a754adb848790b9c02bb81fb0a14e806
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2025-01-21 19:44:13 +0200

    Update 01.py

--- Python/01.py
+++ Python/01.py
@@ -8,6 +8,21 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel(r"scripts\data.xlsx")
 
+df = pd.read_csv("data.csv")
+
+
+# Plot horizontal bars for 2004 and 2022
+plt.barh(y=df['Country'], width=df['2004'], color='#33608C', label='2004')
+plt.barh(y=df['Country'], width=df['2022'], left=df['2004'], color='#BE2641', label='2022')
+
+# Add labels and title
+plt.xlabel('Count')
+plt.title('Comparison of Counts in 2004 and 2022')
+plt.legend()
+
+# Show the plot
+plt.tight_layout()
+plt.show()
 
 
 
